e0a815d0b1d6a10dbaad1a305647e41971181115
Damien Ayers <damien@omad.net>
2011-08-30 14:48:19 +1000

    Small admin improvements

--- cat/admin.py
+++ cat/admin.py
@@ -2,13 +2,11 @@ from django.contrib import admin
 from models import MuseumObject,FunctionalCategory
 
 class MOAdmin(admin.ModelAdmin):
-    fields = ('registration_number','country','description','comment')
+    list_display = ('registration_number','country','description','comment',)
 
-    list_display = ('registration_number','country','description','comment')
+    list_filter = ('country','functional_category', 'loan_status', 'cultural_bloc',)
 
-    list_filter = ('country','functional_category')
-
-    search_fields = ['description','comment']
+    search_fields = ['registration_number', 'description','comment']
 
 
 admin.site.register(MuseumObject, MOAdmin)
