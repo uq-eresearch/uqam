ec2abf6279956a28e2a9173673fa355a4820a055
Damien Ayers <damien@omad.net>
2011-09-01 17:39:31 +1000

    Include some demo views for the public site.

--- cat/views.py
+++ cat/views.py
@@ -1,1 +1,32 @@
 # Create your views here.
+
+import django_tables2 as tables
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django_tables2 import RequestConfig
+from models import MuseumObject
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the catalogue index.")
+
+
+class SimpleTable(tables.Table):
+    class Meta:
+        model = MuseumObject
+        attrs = {'class': 'paleblue'}
+
+def table(request):
+    queryset = MuseumObject.objects.all()
+    table = SimpleTable(queryset)
+    RequestConfig(request, paginate={"per_page": 4}).configure(table)
+    return render_to_response("simple_list.html", {"table": table},
+                              context_instance=RequestContext(request))
+
+def detail(request, artefact_id):
+    a = get_object_or_404(MuseumObject, pk=artefact_id)
+#    try:
+#        a = MuseumObject.objects.get(pk=artefact_id)
+#    except MuseumObject.DoesNotExist:
+#        raise Http404
+    return render_to_response('detail.html', {'artefact': a})
--- settings.py
+++ settings.py
@@ -97,6 +97,16 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request",
+)
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -127,6 +137,7 @@ INSTALLED_APPS = (
     'cat',
     'south',
     'django_extensions',
+    'django_tables2',
 )
 
 # A sample logging configuration. The only tangible logging
