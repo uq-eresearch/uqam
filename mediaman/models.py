8c26acd0e884edcb691d7c10ebb67538d1fd50fd
Damien Ayers <damien@omad.net>
2011-09-14 17:14:26 +1000

    Greatly improve image display. Add link for finding objects with images.

--- mediaman/models.py
+++ mediaman/models.py
@@ -5,6 +5,6 @@ from cat.models import MuseumObject
 # Create your models here.
 
 class ArtefactRepresentation(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, blank=True)
     image = ImageWithThumbsField(upload_to='mediareps/', sizes=((64,64),(400,400)))
     artefact = models.ForeignKey(MuseumObject)
