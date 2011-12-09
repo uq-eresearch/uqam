28fcd62f699c41e226915c556d618d1e59376d37
Damien Ayers <damien@omad.net>
2011-12-09 10:33:16 +1000

    Improve fab file, allow reseting/reloading remote database.

--- fabfile.py
+++ fabfile.py
@@ -18,7 +18,7 @@ def upgrade():
     reqs()
     _collectstatic()
     _syncdb()
-    reload()
+    reload_servers()
 
 
 def bootstrap():
@@ -54,7 +54,7 @@ def push():
         run('tar xjf %s' % filename)
 
 def _collectstatic():
-    _virtualenv('./manage.py collectstatic --noinput')
+    _venv('./manage.py collectstatic --noinput')
 
 def _pack():
     filename = '/tmp/uqam.tar.bz2'
@@ -64,18 +64,23 @@ def _pack():
 
 def _syncdb():
     """Migrate the remote database to the latest version"""
-    _virtualenv('./manage.py syncdb')
-    _virtualenv('./manage.py migrate')
+    _venv('./manage.py syncdb')
+    _venv('./manage.py migrate')
+
+def resetcat():
+    """DANGER: Wipe/Reset the remote catalogue"""
+    _venv('./manage.py reset cat loans condition')
+    _venv('./manage.py migrate --fake')
 
 def importcat():
     """Remotely import the catalogue and images"""
     classificationsfile = '/home/django/origdb/ClassificationsNov11.xlsx'
-#    _virtualenv('./manage.py importcategories %s' % classificationsfile)
-#    _virtualenv('./manage.py importcat /home/django/origdb cat loans condition')
-    _virtualenv('./manage.py importxls /home/django/origdb/Museum.xlsx')
+    _venv('./manage.py importcategories %s' % classificationsfile)
+    _venv('./manage.py importcat /home/django/origdb cat loans condition')
+    _venv('./manage.py importxls /home/django/origdb/Museum.xlsx')
 
 def importimages():
-    _virtualenv('./manage.py importmedia /home/django/images')
+    _venv('./manage.py importmedia /home/django/images')
 
 def copyimages():
     local('rsync -rv %s(imagesdir) django@anthropology-uat:images')
@@ -89,12 +94,12 @@ def copyimages():
 #    put(db, '/tmp/db.mdb')
 #    run('./manage.py importcat pathtomdb')
 
-def _virtualenv(cmd):
+def _venv(cmd):
     with cd(env.appdir):
         with prefix('source %(virtenv)s/bin/activate' % env):
             run(cmd)
 
-def reload():
+def reload_servers():
     with settings(user='uqdayers'):
         sudo('service nginx reload')
         sudo('service celeryd reload')
@@ -104,16 +109,17 @@ def reload():
 
 def rebuild_index():
     """Rebuild the entire search index"""
-    _virtualenv('./manage.py rebuild_index')
+    _venv('./manage.py rebuild_index')
 
 def update_index():
     """Update search index"""
-    _virtualenv('./manage.py update_index')
+    _venv('./manage.py update_index')
 
 def test_upgrade():
     temp_archive = "/tmp/current.tar.gz"
     run('rm -f %s' % temp_archive)
-    run('tar czf %s --exclude=media %s uqam.db' % (temp_archive, env.appname))
+    run('tar czf %s --exclude=media %s uqam.db' %
+            (temp_archive, env.appname))
     get(temp_archive, temp_archive)
 
     with lcd('/tmp'):
@@ -130,6 +136,11 @@ def test_upgrade():
 #        local('rm -rf ../public; mkdir -p ../public/static ../public/media')
 #        local('./manage.py collectstatic --noinput')
         local('./manage.py runserver 0.0.0.0:8000')
+
+def reset_south():
+    local("echo 'delete from south_migrationhistory' | manage.py dbshell")
+    local("./manage.py reset --noinput cat loans condition")
+    local("./manage.py migrate --fake")
 
 def shell():
     open_shell()
