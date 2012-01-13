a01824cf61d51812ff21f58f2eb7e40fca1d9ac4
Damien Ayers <uqdayers@uqdayers-OptiPlex-980.(none)>
2012-01-13 17:03:00 +1000

    Changed dev environment

--- dev_settings.py
+++ dev_settings.py
@@ -53,6 +53,6 @@ DEBUG_TOOLBAR_CONFIG = {
 
 MEDIA_ROOT = os.path.join(DIRNAME, 'media')
 
-HAYSTACK_WHOOSH_PATH = '/home/omad/whoosh/cat_index'
+HAYSTACK_WHOOSH_PATH = '/home/uqdayers/whoosh/cat_index'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
