d8e32039ebaab11ec71b02b13fea7d2ab6b0126b
Damien Ayers <damien@omad.net>
2012-02-07 15:41:37 +1000

    Fix pep8 styling

--- cat/models.py
+++ cat/models.py
@@ -32,15 +32,15 @@ class MuseumObject(models.Model):
     place = models.ForeignKey('Place', null=True, db_index=True)
 
     donor = models.ForeignKey(
-            'Person', 
-            null=True, 
-            blank=True, 
+            'Person',
+            null=True,
+            blank=True,
             db_index=True,
             related_name="donated_objects")
     donor_2 = models.ForeignKey(
-            'Person', 
-            null=True, 
-            blank=True, 
+            'Person',
+            null=True,
+            blank=True,
             related_name="donated_objects_2")
     how_donor_obtained = models.CharField(max_length=50, blank=True)
     ## TODO: when_donor_obtained should be DateField
@@ -48,15 +48,15 @@ class MuseumObject(models.Model):
 
     photographer = models.CharField(max_length=100)
     collector = models.ForeignKey(
-            'Person', 
-            null=True, 
-            blank=True, 
+            'Person',
+            null=True,
+            blank=True,
             db_index=True,
             related_name="collected_objects")
     collector_2 = models.ForeignKey(
-            'Person', 
-            null=True, 
-            blank=True, 
+            'Person',
+            null=True,
+            blank=True,
             related_name="collected_objects_2")
     how_collector_obtained = models.CharField(max_length=50, blank=True)
     when_collector_obtained = models.CharField(max_length=50, blank=True)
@@ -78,35 +78,35 @@ class MuseumObject(models.Model):
     is_public_comment = models.BooleanField(default=False)
     comment = models.TextField(blank=True)
     private_comment = models.TextField(
-            blank=True, 
+            blank=True,
             help_text="Only visible to staff")
     significance = models.TextField("statement of significance",
             blank=True)
 
-    width = models.IntegerField(null=True,blank=True)
-    length = models.IntegerField(null=True,blank=True)
-    height = models.IntegerField(null=True,blank=True)
-    depth = models.IntegerField(null=True,blank=True)
-    circumference = models.IntegerField(null=True,blank=True)
+    width = models.IntegerField(null=True, blank=True)
+    length = models.IntegerField(null=True, blank=True)
+    height = models.IntegerField(null=True, blank=True)
+    depth = models.IntegerField(null=True, blank=True)
+    circumference = models.IntegerField(null=True, blank=True)
 
-    longitude = models.FloatField(null=True,blank=True)
-    latitude = models.FloatField(null=True,blank=True)
-    
+    longitude = models.FloatField(null=True, blank=True)
+    latitude = models.FloatField(null=True, blank=True)
+
     related_documents = models.ManyToManyField(
-            'mediaman.Document', 
-            related_name='related_museumobjects', 
+            'mediaman.Document',
+            related_name='related_museumobjects',
             null=True, blank=True)
 
     def dimensions(self):
         '''Returns textual dimensions for the item
-        
+
         Combining length, width, height, depth, circumference
         '''
-        dims = [self.length, self.width, self.height, self.depth, self.circumference]
-        dims = [str(d/10) for d in dims if d != None]
+        dims = [self.length, self.width, self.height,
+                self.depth, self.circumference]
+        dims = [str(d / 10) for d in dims if d != None]
         s = string.join(dims, " x ")
         return s + " cm" if s else ''
-
 
     @models.permalink
     def get_absolute_url(self):
@@ -122,8 +122,6 @@ class MuseumObject(models.Model):
     def autocomplete_search_fields():
         return ("id__iexact",)
 
-    
-
 
 class FunctionalCategory(models.Model):
     name = models.CharField('function category', max_length=30, unique=True)
@@ -136,16 +134,21 @@ class FunctionalCategory(models.Model):
         verbose_name_plural = "Functional categories"
         ordering = ['name']
 
+
 class CulturalBloc(models.Model):
     name = models.CharField(max_length=30, db_index=True, unique=True)
     definition = models.TextField(blank=True)
+
     @models.permalink
     def get_absolute_url(self):
         return ('culturalbloc_detail', [str(self.name)])
+
     def __unicode__(self):
         return self.name
+
     class Meta:
         ordering = ['name']
+
 
 class ArtefactType(models.Model):
     """
@@ -156,10 +159,13 @@ class ArtefactType(models.Model):
     name = models.CharField(max_length=150, unique=True)
     definition = models.TextField(blank=True)
     see_also = models.CharField(max_length=150, blank=True)
+
     def __unicode__(self):
         return self.name
+
     class Meta:
         ordering = ['name']
+
 
 class Place(models.Model):
     country = models.CharField(max_length=30, blank=True)
@@ -167,32 +173,41 @@ class Place(models.Model):
     australian_state = models.CharField(max_length=20, blank=True)
     name = models.CharField(max_length=150)
 
-    is_corrected = models.BooleanField(default=False, 
-            help_text="Has someone manually moved the marker to it's correct location.")
-    gn_name = models.CharField(max_length=100, help_text="GeoNames Name", blank=True)
-    gn_id = models.CharField(max_length=20, help_text="GeoNames ID", blank=True)
+    is_corrected = models.BooleanField(default=False,
+            help_text="Has someone manually"
+            "moved the marker to it's correct location.")
+    gn_name = models.CharField(max_length=100,
+            help_text="GeoNames Name", blank=True)
+    gn_id = models.CharField(max_length=20,
+            help_text="GeoNames ID", blank=True)
     latitude = models.FloatField(blank=True, null=True)
     longitude = models.FloatField(blank=True, null=True)
+
     class Meta:
         ordering = ["id"]
+
     def __unicode__(self):
         return ' > '.join([self.country, self.region, self.name])
+
     @models.permalink
     def get_absolute_url(self):
         return ('place_detail', [str(self.id)])
+
     def get_geonames_url(self):
         if self.gn_id:
             return "http://www.geonames.org/%s" % self.gn_id
         else:
             return False
+
     def get_kml_coordinates(self):
         return "%s,%s,0" % (self.longitude, self.latitude)
-    def geocode(self, force=False):
+
+    def geocode_net(self, force=False):
         """
         Lookup the latitude and longitude of this place with GeoNames
 
         Place must be saved after use. Set `force` to re-lookup the location.
-        
+
         Can take a few seconds to return, since this uses a network request.
         """
         if self.gn_id and not force:
@@ -207,6 +222,19 @@ class Place(models.Model):
         self.latitude = lat
         self.longitude = lng
 
+    def geocode(self, force=False):
+        from geonames.models import Geoname
+        country = Geoname.objects.countries(name__contains=self.country)[0]
+        geonames = Geoname.objects.filter(name__icontains=self.name,
+                country=country.country)
+        if geonames:
+            g = geonames[0]
+            self.gn_name = g.name
+            self.gn_id = g.geonameid
+            p = g.point.split(',')
+            self.longitude = float(p[0])
+            self.latitude = float(p[1])
+
 
 class Person(models.Model):
     """
@@ -215,19 +243,23 @@ class Person(models.Model):
     name = models.CharField(max_length=150)
     comments = models.TextField(blank=True)
     related_documents = models.ManyToManyField('mediaman.Document',
-            related_name='related_people',blank=True)
+            related_name='related_people', blank=True)
 
     @models.permalink
     def get_absolute_url(self):
         return ('person_detail', [str(self.id)])
+
     def __unicode__(self):
         return self.name
+
     class Meta:
         ordering = ['name']
         verbose_name_plural = "People"
+
     @staticmethod
     def autocomplete_search_fields():
         return ("name__iexact",)
+
 
 class Region(models.Model):
     name = models.CharField(max_length=60, unique=True)
@@ -235,6 +267,7 @@ class Region(models.Model):
 
     def __unicode__(self):
         return self.name
+
 
 class Category(models.Model):
     """
@@ -250,10 +283,12 @@ class Category(models.Model):
         if self.parent:
             return self.parent.__unicode__() + " :: " + self.name
         return self.name
+
     class Meta:
         ordering = ['name']
         verbose_name_plural = "Categories"
         unique_together = (("slug", "parent"), ("name", "parent"))
+
     def get_absolute_url(self):
         url = "/%s/" % self.slug
         category = self
@@ -262,7 +297,6 @@ class Category(models.Model):
             category = category.parent
         url = "/categories" + url
         return url
-
 
 
 class Maker(models.Model):
