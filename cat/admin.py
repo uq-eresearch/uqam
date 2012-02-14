14d9c9293ec222aa4e94215483b520fec453eae6
Damien Ayers <damien@omad.net>
2012-02-14 15:43:31 +1000

    Fix pep8

--- cat/admin.py
+++ cat/admin.py
@@ -5,11 +5,11 @@ from mediaman.models import ArtefactRepresentation
 from common.admin import UndeleteableModelAdmin
 from cat.adminactions import merge_selected, add_to_collection
 
+
 class ArtefactRepInline(admin.TabularInline):
     model = ArtefactRepresentation
-    search_fields = ['name',]
+    search_fields = ['name', ]
 #    classes = ('collapse closed',)
-
 
 
 class MOAdmin(UndeleteableModelAdmin):
@@ -17,7 +17,7 @@ class MOAdmin(UndeleteableModelAdmin):
                     'description', 'comment',)
     actions = [add_to_collection]
 
-    list_filter = ('place__country','functional_category__name',
+    list_filter = ('place__country', 'functional_category__name',
                     'access_status', 'loan_status', 'cultural_bloc',
                     'artefact_type', 'collector__name', 'donor__name')
 
@@ -52,7 +52,8 @@ class MOAdmin(UndeleteableModelAdmin):
         }),
         ('Storage location', {
             'classes': ('collapse',),
-            'fields': (('storage_section', 'storage_unit',), ('storage_bay', 'storage_shelf_box_drawer'),)
+            'fields': (('storage_section', 'storage_unit',),
+                ('storage_bay', 'storage_shelf_box_drawer'),)
         }),
         ('Acquisition', {
             'classes': ('collapse',),
@@ -61,11 +62,13 @@ class MOAdmin(UndeleteableModelAdmin):
         }),
         ('Collector', {
             'classes': ('collapse',),
-            'fields': ('collector', 'collector_2', 'how_collector_obtained', 'when_collector_obtained')
+            'fields': ('collector', 'collector_2',
+                'how_collector_obtained', 'when_collector_obtained')
         }),
         ('Donor', {
             'classes': ('collapse',),
-            'fields': ('donor', 'donor_2', 'how_donor_obtained', 'when_donor_obtained')
+            'fields': ('donor', 'donor_2',
+                'how_donor_obtained', 'when_donor_obtained')
         }),
         ('Details', {
             'classes': ('collapse',),
@@ -84,7 +87,8 @@ class MOAdmin(UndeleteableModelAdmin):
         }),
         ('Dimensions', {
             'classes': ('collapse',),
-            'fields': (('length', 'width', 'height'), ('depth', 'circumference'))
+            'fields': (('length', 'width', 'height'),
+                ('depth', 'circumference'))
         }),
         ('Related documents', {
             'classes': ('collapse',),
@@ -93,64 +97,73 @@ class MOAdmin(UndeleteableModelAdmin):
     )
 
 
-
 admin.site.register(MuseumObject, MOAdmin)
-
 
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'comments',)
-    search_fields = ['name', 'comments',]
+    search_fields = ['name', 'comments']
     filter_horizontal = ['related_documents']
     actions = [merge_selected]
 admin.site.register(Person, PersonAdmin)
 
+
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'country', 'region', 'australian_state', 'name', 'gn_name')
+    list_display = ('id', 'country', 'region', 'australian_state',
+            'name', 'gn_name')
     list_filter = ('country', 'australian_state', 'region',)
-    
+
     def geocode_place(modeladmin, request, queryset):
         from cat.tasks import GeocodePlace
         for place in queryset:
             GeocodePlace.delay(place.id)
     geocode_place.short_description = "Lookup latitude/longitude"
 
+    def geocode_local(modeladmin, request, queryset):
+        pass
+
     actions = [merge_selected, geocode_place]
-    search_fields = ['country', 'region__name','australian_state', 'name']
+    search_fields = ['country', 'region__name', 'australian_state', 'name']
 admin.site.register(Place, PlaceAdmin)
+
 
 class FunctionCategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'definition')
-    search_fields = ['name',]
+    search_fields = ['name']
     actions = [merge_selected]
 admin.site.register(FunctionalCategory, FunctionCategoryAdmin)
 
+
 class CulturalBlocAdmin(admin.ModelAdmin):
     list_display = ('name', 'definition')
-    search_fields = ['name',]
+    search_fields = ['name', ]
     actions = [merge_selected]
 admin.site.register(CulturalBloc, CulturalBlocAdmin)
+
 
 class ArtefactRepresentationAdmin(admin.ModelAdmin):
     actions = [merge_selected]
     raw_id_fields = ('artefact',)
 admin.site.register(ArtefactRepresentation, ArtefactRepresentationAdmin)
 
+
 class ArtefactTypeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'definition')
-    search_fields = ['name',]
+    search_fields = ['name']
     actions = [merge_selected]
 admin.site.register(ArtefactType, ArtefactTypeAdmin)
+
 
 class RegionAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     search_fields = ['name', 'description']
 admin.site.register(Region, RegionAdmin)
 
+
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
     search_fields = ('name', 'slug')
 admin.site.register(Category, CategoryAdmin)
+
 
 class MakerAdmin(admin.ModelAdmin):
     list_display = ('name', 'comment')
