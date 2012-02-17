e95eabdd831d170bdf5e45a0a4123ef460b0b360
Damien Ayers <damien@omad.net>
2012-02-17 16:48:25 +1000

    Clean up loans data structure a bit.

--- loans/models.py
+++ loans/models.py
@@ -41,6 +41,7 @@ class LoanAgreement(models.Model):
 
 class LoanPurpose(models.Model):
     purpose = models.CharField(max_length=20)
+    definition = models.CharField(max_length=100)
 
     def __unicode__(self):
         return self.purpose
