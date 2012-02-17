aef5c41b09f74e096c4697ddfc6a765c83c3992b
Damien Ayers <damien@omad.net>
2012-02-17 14:24:42 +1000

    Improve loans modelling

--- loans/models.py
+++ loans/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from cat.models import MuseumObject
 
+RETURNED_OPTIONS = [
+    ('Yes', 'Yes'),
+    ('No', 'No'),
+    ('Extended', 'Extended')
+    ]
+
+LOAN_TYPES = [
+    ('Internal', 'Internal'),
+    ('External', 'External')
+    ]
+
 
 class LoanAgreement(models.Model):
     ref = models.CharField(max_length=10)
@@ -11,10 +22,14 @@ class LoanAgreement(models.Model):
             related_name="approved_by")
     prepared_by = models.ForeignKey('MuseumStaff',
             related_name="prepared_by")
-    loan_type = models.CharField(max_length=20)
-    loan_purpose = models.CharField(max_length=20)
-    special_loan_conditions = models.TextField()
-    comments = models.TextField()
+    loan_type = models.CharField(blank=True, max_length=20,
+            choices=LOAN_TYPES)
+    special_loan_conditions = models.TextField(blank=True)
+    location = models.CharField(blank=True, max_length=200)
+    purpose = models.ForeignKey('LoanPurpose', null=True)
+    returned = models.CharField(blank=True, max_length=100,
+            choices=RETURNED_OPTIONS)
+    comments = models.TextField(blank=True)
     items = models.ManyToManyField(MuseumObject, through='LoanItem')
 
     class Meta:
@@ -23,11 +38,22 @@ class LoanAgreement(models.Model):
     def __unicode__(self):
         return "LoanAgreement " + str(self.id) + " " + self.ref
 
+
+class LoanPurpose(models.Model):
+    purpose = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.purpose
+
+
 class LoanItem(models.Model):
     loan = models.ForeignKey(LoanAgreement)
     item = models.ForeignKey(MuseumObject)
-    out_condition = models.CharField(max_length=30)
-    return_condition = models.CharField(max_length=30)
+    out_condition = models.CharField(blank=True, max_length=30)
+    return_condition = models.CharField(blank=True, max_length=30)
+
+    class Meta:
+        unique_together = ('loan', 'item')
 
     def __unicode__(self):
         return self.item.__unicode__()
@@ -35,20 +61,23 @@ class LoanItem(models.Model):
 
 class Client(models.Model):
     name = models.CharField(max_length=200)
-    organisation = models.CharField(max_length=80)
-    address = models.CharField(max_length=300)
-    town_suburb = models.CharField(max_length=80)
-    state = models.CharField(max_length=30)
-    postcode = models.CharField(max_length=10)
+    organisation = models.CharField(blank=True, max_length=80)
+    position = models.CharField(blank=True, max_length=80)
+    address = models.TextField(blank=True)
+    town_suburb = models.CharField(blank=True, max_length=80)
+    state = models.CharField(blank=True, max_length=30)
+    postcode = models.CharField(blank=True, max_length=10)
+    country = models.CharField(blank=True, max_length=50)
 
-    phone1 = models.CharField(max_length=20)
-    phone2 = models.CharField(max_length=20)
+    phone1 = models.CharField(blank=True, max_length=20)
+    phone2 = models.CharField(blank=True, max_length=20)
 
     class Meta:
         ordering = ['name']
 
     def __unicode__(self):
         return self.name
+
 
 class MuseumStaff(models.Model):
     name = models.CharField(max_length=200)
