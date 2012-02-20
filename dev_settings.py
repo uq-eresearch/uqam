630ac8c72680aab25d474a314a87e6f5ceb0b91b
Damien Ayers <damien@omad.net>
2012-02-20 11:07:32 +1000

    cleanup dev database settings

--- dev_settings.py
+++ dev_settings.py
@@ -9,33 +9,21 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 DATABASES = {
- #   'default': {
- #       'ENGINE': 'django.db.backends.sqlite3',
- # # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
- #       'NAME': 'uqam.db',                      # Or path to database file if using sqlite3.
- #       'USER': '',                      # Not used with sqlite3.
- #       'PASSWORD': '',                  # Not used with sqlite3.
- #       'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
- #       'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
- #       },
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'uqam_new',                      # Or path to database file if using sqlite3.
-        'USER': 'uqam',                      # Not used with sqlite3.
-        'PASSWORD': 'uqam',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'NAME': 'uqam',
+        'USER': 'uqam',
+        'PASSWORD': 'uqam',
+        'HOST': 'localhost',
+        'PORT': '',
     },
     'readonly': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-# Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'uqam_new',         # Or path to database file if using sqlite3.
-        'USER': 'uqam_read',                      # Not used with sqlite3.
-        'PASSWORD': 'uqam_read',                  # Not used with sqlite3.
-        'HOST': 'localhost',  # Set to empty string for localhost.
+        'NAME': 'uqam',
+        'USER': 'uqam_read',
+        'PASSWORD': 'uqam_read',
+        'HOST': 'localhost',
         'PORT': '',
-        # Set to empty string for default. Not used with sqlite3.
     },
 }
 
