cda351c19658b82a5c23de4eb9a481d6b872c046
Damien Ayers <damien@omad.net>
2012-02-21 11:22:23 +1000

    Fabfile pep8

--- fabfile.py
+++ fabfile.py
@@ -26,6 +26,7 @@ def bootstrap():
     run('virtualenv --no-site-packages %(virtenv)s' % env)
     reqs()
 
+
 def init_celery():
     put('etc/init-celery.conf', '/tmp/celery.conf')
     put('etc/init-celeryd', '/tmp/celeryd')
@@ -37,14 +38,17 @@ def init_celery():
         sudo('chmod 755 /etc/init.d/celeryd')
         sudo('service celeryd restart')
 
+
 def installsyspackages():
     with settings(user='uqdayers'):
         sudo('yum install openldap-devel openssl-devel')
+
 
 def reqs():
     """Update the remote virtualenv to newest requirements"""
     with prefix('source %(virtenv)s/bin/activate' % env):
         run('pip install --requirement=%(reqfile)s' % env)
+
 
 def push():
     """Deploy the newest source to the server"""
@@ -53,8 +57,10 @@ def push():
     with cd(env.appdir):
         run('tar xjf %s' % filename)
 
+
 def _collectstatic():
     _venv('./manage.py collectstatic --noinput')
+
 
 def _pack():
     filename = '/tmp/uqam.tar.bz2'
@@ -67,10 +73,12 @@ def _syncdb():
     _venv('./manage.py syncdb')
     _venv('./manage.py migrate')
 
+
 def resetcat():
     """DANGER: Wipe/Reset the remote catalogue"""
     _venv('./manage.py reset cat loans condition')
     _venv('./manage.py migrate --fake')
+
 
 def importcat():
     """Remotely import the catalogue and images"""
@@ -79,8 +87,10 @@ def importcat():
     _venv('./manage.py importcat /home/django/origdb cat loans condition')
     _venv('./manage.py importxls /home/django/origdb/Museum.xlsx')
 
+
 def importimages():
     _venv('./manage.py importmedia /home/django/images')
+
 
 def copyimages():
     local('rsync -rv %s(imagesdir) django@anthropology-uat:images')
@@ -94,10 +104,12 @@ def copyimages():
 #    put(db, '/tmp/db.mdb')
 #    run('./manage.py importcat pathtomdb')
 
+
 def _venv(cmd):
     with cd(env.appdir):
         with prefix('source %(virtenv)s/bin/activate' % env):
             run(cmd)
+
 
 def reload_servers():
     with settings(user='uqdayers'):
@@ -111,9 +123,11 @@ def rebuild_index():
     """Rebuild the entire search index"""
     _venv('./manage.py rebuild_index')
 
+
 def update_index():
     """Update search index"""
     _venv('./manage.py update_index')
+
 
 def test_upgrade():
     temp_archive = "/tmp/current.tar.gz"
@@ -132,21 +146,25 @@ def test_upgrade():
         local('sed -i -r -e "s/\\/home\\/django/\\/tmp/g" default_settings.py')
         local('./manage.py syncdb')
         local('./manage.py migrate')
-        local('sed -i -r -e "s/DEBUG = False/DEBUG = True/g" default_settings.py')
+        local('sed -ire "s/DEBUG = False/DEBUG = True/g" default_settings.py')
 #        local('rm -rf ../public; mkdir -p ../public/static ../public/media')
 #        local('./manage.py collectstatic --noinput')
         local('./manage.py runserver 0.0.0.0:8000')
+
 
 def reset_south():
     local("echo 'delete from south_migrationhistory' | manage.py dbshell")
     local("./manage.py reset --noinput cat loans condition")
     local("./manage.py migrate --fake")
 
+
 def shell():
     open_shell()
 
+
 def setup_local_database():
     local()
+
 
 def docs():
     with lcd('docs'):
