737cab001d1bb916d772a584767c782bf2e54a04
Damien Ayers <damien@omad.net>
2012-02-22 17:00:17 +1000

    Fix copyright message on docs

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'uqam'
-copyright = u'2011, Damien Ayers'
+copyright = u'2012, University of Queensland'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
