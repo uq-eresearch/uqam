bf816b3d4104ae2ca10675dfe0d8115bdab76f65
Damien Ayers <damien@omad.net>
2012-05-11 17:21:57 +1000

    super basic unit testing begining

--- mediaman/tests.py
+++ mediaman/tests.py
@@ -5,7 +5,9 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+#from django.test import TestCase
+from unittest import TestCase
+from views import name_to_id
 
 
 class SimpleTest(TestCase):
@@ -14,3 +16,26 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+    def assertNameToId(self, id, name, path=None):
+        testid = name_to_id(name, path)
+        self.assertEqual(id, testid)
+
+    def test_name_to_id(self):
+        self.assertNameToId(1234, '1234.jpg')
+
+        self.assertNameToId(12345, '12345.jpg')
+
+        self.assertNameToId(12345, '12345_2.jpg')
+
+        self.assertNameToId(12345, '12345.pdf')
+        self.assertNameToId(12345, '12345.png')
+        self.assertNameToId(12345, '12345.tiff')
+
+    def test_name_to_id_with_path(self):
+        self.assertNameToId(12345, 'condition.tiff',
+                'S:\\scanneddocs\\12345\\')
+
+        self.assertNameToId(12345, 'condition.tiff',
+                '/home/omad/files/12345/')
+
--- settings.py
+++ settings.py
@@ -4,3 +4,6 @@ if os.path.exists('development_mode'):
     from dev_settings import *
 else:
     from default_settings import *
+
+
+TEST_RUNNER = 'common.testrunner.NoDbTestRunner'
