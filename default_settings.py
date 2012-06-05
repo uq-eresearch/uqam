00e6008fb50aa96f8571f69fb6e2e32a2621cb48
Damien Ayers <damien@omad.net>
2012-06-05 17:11:35 +1000

    add better logging config

--- default_settings.py
+++ default_settings.py
@@ -168,6 +168,10 @@ INSTALLED_APPS = (
     'easy_thumbnails',
 )
 
+LOGS_ROOT = os.path.join(DIRNAME, 'logs')
+backup_count = 1000
+if DEBUG:
+    backup_count = 2
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
@@ -176,17 +180,68 @@ INSTALLED_APPS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+            'class': 'django.utils.log.AdminEmailHandler',
+        },
+        'logfile': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(LOGS_ROOT, "uqam.log"),
+            'maxBytes': 50000,
+            'backupCount': backup_count,
+            'formatter': 'standard',
+        },
+        'errorlog': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(LOGS_ROOT, "uqam.log"),
+            'maxBytes': 50000,
+            'backupCount': backup_count,
+            'formatter': 'standard',
+        },
+        'db': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(LOGS_ROOT, "db.log"),
+            'maxBytes': 50000,
+            'backupCount': backup_count,
+            'formatter': 'standard',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
     },
     'loggers': {
+        'django': {
+            'handlers': ['console', 'errorlog'],
+            'propagate': True,
+            'level': 'WARN',
+        },
+        'django.db.backends': {
+            'handlers': ['db'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
-            'propagate': True,
+            'propagate': False,
+        },
+        #Default handler for everything that we're doing. Hopefully this doesn't double-print
+        #the Django things as well. Not 100% sure how logging works :)
+        '': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
         },
     }
 }
