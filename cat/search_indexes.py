ffe0951714887105da4fabedaaaee965555d5757
Damien Ayers <damien@omad.net>
2012-07-09 17:08:08 +1000

    search index update

--- cat/search_indexes.py
+++ cat/search_indexes.py
@@ -6,6 +6,17 @@ from .models import MuseumObject
 class MuseumObjectIndex(SearchIndex):
     text = CharField(document=True, use_template=True)
 
+    def get_model(self):
+        return MuseumObject
+
+    def index_queryset(self):
+        """
+        Used when the entire index for model is updated.
+        """
+        ### TODO ###
+        # Ignore private/reserved etc objects
+        return self.get_model().objects.all()
+
 site.register(MuseumObject, MuseumObjectIndex)
 
 #class PersonIndex(SearchIndex):
