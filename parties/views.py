8533c934a3c4b4c849f074d2d3a0e8ff1da44c54
Damien Ayers <damien@omad.net>
2012-07-23 09:48:03 +1000

    add query for new people page design

--- parties/views.py
+++ parties/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+
+
+def people_aggregate(request):
+    people = Person.objects.raw("""
+        select id, name,
+          (select count(id) from cat_museumobject where donor_id = parties_person.id) as donated_count,
+          (select count(id) from cat_museumobject where collector_id = parties_person.id) as collected_count,
+          (select count(id) from parties_person_related_documents where person_id = parties_person.id) as documents_count
+        from parties_person
+        """)
