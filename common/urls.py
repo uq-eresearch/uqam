0e26b039330502130832d52eb30cbd517a3276eb
Damien Ayers <damien@omad.net>
2012-08-07 14:39:57 +1000

    cleanup search urls.py

--- common/urls.py
+++ common/urls.py
@@ -3,14 +3,6 @@ from haystack.forms import FacetedSearchForm
 from haystack.query import SearchQuerySet
 from views import PersistentSearchView
 
-#from haystack.views import search_view_factory
-#urlpatterns = patterns('haystack.views',
-#    url(r'^search/', search_view_factory(
-#        view_class=PersistentSearchView
-#        ), name='haystack_search'),
-#)
-
-
 sqs = SearchQuerySet().facet('categories').facet('people').facet('country').facet('has_images').facet('global_region')
 
 
