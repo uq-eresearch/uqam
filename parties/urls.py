6d991572f0f0938eddbe4e4bf554fefa66e3ebce
Damien Ayers <damien@omad.net>
2012-08-22 17:27:10 +1000

    Start datatables person view

--- parties/urls.py
+++ parties/urls.py
@@ -1,3 +1,9 @@
 from django.conf.urls.defaults import *
+from models import Person, Maker
+from django.views.generic import DetailView, ListView
 
 # place app url patterns here
+
+urlpatterns = patterns('parties.views',
+    url('browse/', 'people_aggregate', name='parties_browse'),
+)
--- parties/views.py
+++ parties/views.py
@@ -1,11 +1,14 @@
-# Create your views here.
+from django.shortcuts import render
+from parties.models import Person
 
 
-def people_aggregate(request):
+def people_aggregate(request, template_name='parties/datatables.html'):
     people = Person.objects.raw("""
-        select id, name,
+        select id, display_name,
           (select count(id) from cat_museumobject where donor_id = parties_person.id) as donated_count,
           (select count(id) from cat_museumobject where collector_id = parties_person.id) as collected_count,
           (select count(id) from parties_person_related_documents where person_id = parties_person.id) as documents_count
         from parties_person
         """)
+    return render(request, template_name,
+            {'person_list': people})
