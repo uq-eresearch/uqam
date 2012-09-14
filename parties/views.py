d2a32cb9ee06125ccb15e61adb24384c3cc4c27c
Damien Ayers <damien@omad.net>
2012-09-14 17:02:41 +1000

    Only count public objects

--- parties/views.py
+++ parties/views.py
@@ -7,9 +7,9 @@ from utils.utils import do_paging
 def people_aggregate(request, template_name='parties/datatables.html'):
     people = Person.objects.raw("""
         select id, display_name, name,
-          (select count(id) from cat_museumobject where donor_id = parties_person.id) as donated_count,
-          (select count(id) from cat_museumobject where collector_id = parties_person.id) as collected_count,
-          (select count(id) from cat_museumobject where maker_id = parties_person.id) as created_count,
+          (select count(id) from cat_museumobject where donor_id = parties_person.id and public = true) as donated_count,
+          (select count(id) from cat_museumobject where collector_id = parties_person.id and public = true) as collected_count,
+          (select count(id) from cat_museumobject where maker_id = parties_person.id and public = true) as created_count,
           (select count(id) from parties_person_related_documents where person_id = parties_person.id) as documents_count
         from parties_person
         """)
@@ -22,9 +22,9 @@ def person_detail(request, pk, item_type='default', template_name='parties/perso
     person = get_object_or_404(Person, pk=int(pk))
 
     mapping = {'docs': ('related_documents', 'Related documents', person.related_documents.count()),
-               'created': ('created_items', 'Created items', person.created_items.count()),
-               'collected': ('collected_objects', 'Collected items', person.collected_objects.count()),
-               'donated': ('donated_objects', 'Donated items', person.donated_objects.count())}
+               'created': ('created_items', 'Created items', person.created_items.filter(public=True).count()),
+               'collected': ('collected_objects', 'Collected items', person.collected_objects.filter(public=True).count()),
+               'donated': ('donated_objects', 'Donated items', person.donated_objects.filter(public=True).count())}
 
     if item_type == 'default':
         # Select max count type
