dad5dd4af3a713b696409b1c4d90af603fcad1a8
Damien Ayers <damien@omad.net>
2012-11-28 14:24:29 +1000

    custom settings (domain name) for uat

--- settings.py
+++ settings.py
@@ -1,7 +1,13 @@
 # Django settings for uqam project.
 import os.path
+import socket
+
+hostname = socket.gethostname()
+
 if os.path.exists('development_mode'):
     from dev_settings import *
+elif hostname.endswith('uat'):
+    from uat_settings import *
 else:
     from default_settings import *
 
---
+++ uat_settings.py
@@ -0,0 +1,10 @@
+"""
+Custom settings for UAT server
+
+Will override any default settings if hostname file exists.
+"""
+from default_settings import *
+
+PUBLIC_DOMAIN = 'anthropology-uat.metadata.net'
+SECURE_DOMAIN = 'anthropology-uat.metadata.net'
+XSESSION_DOMAINS = ['http://anthropology-uat.metadata.net']
